a2c4b15e5314dca08fc5677dda9b35d19cdfee1c
Ayman Osman <aymano.osman@gmail.com>
2016-08-30 23:56:16 +0100

    wip

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 "use strict";
 
+var _ = require("lodash");
 var aws = require("aws-sdk");
 
 aws.config.region = "eu-west-1";
@@ -8,22 +9,34 @@ var ec2 = new aws.EC2;
 var input = process.argv.slice(2);
 var command = input.shift();
 
+function handleError(err) {
+  console.error(err);
+}
+
 if (module === require.main) {
 
     switch (command) {
-        case 'status':
-            console.log('wwwwhat??');
+        case "node":
             ec2.describeInstances().promise()
             .then(function(res) {
-                console.log('what??');
+                var instances = _.flatMap(res.Reservations, (r) => r.Instances);
+                function printCollection(coll) {
+                    console.log("Found %s instances", coll.length);
+                }
+                printCollection(instances)
+            }).catch(handleError);
+
+            break;
+
+        case "status":
+            ec2.describeInstances().promise()
+            .then(function(res) {
                 var id = res.Reservations[0].Instances[0].InstanceId;
                 return ec2.describeInstanceStatus({InstanceIds: [id]}).promise();
             }).then(function(res) {
                 var statuses = res.InstanceStatuses[0]
                 console.log('RRR', statuses);
-            }).catch(function(err) {
-                console.error(err);
-            });
+            }).catch(handleError);
             break;
         case 'sss':
 
