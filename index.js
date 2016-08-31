73b04b0c6c177b3d6a3fa3053a89fc9fad6fca42
Ayman Osman <aymano.osman@gmail.com>
2016-08-31 01:15:00 +0100

    wip

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var input = process.argv.slice(2);
 var command = input.shift();
 
 function handleError(err) {
-  console.error(err);
+    console.error(err);
 }
 
 if (module === require.main) {
@@ -18,25 +18,49 @@ if (module === require.main) {
     switch (command) {
         case "node":
             ec2.describeInstances().promise()
-            .then(function(res) {
-                var instances = _.flatMap(res.Reservations, (r) => r.Instances);
-                function printCollection(coll) {
-                    console.log("Found %s instances", coll.length);
-                }
-                printCollection(instances)
-            }).catch(handleError);
+                .then(function(res) {
+                    var instances = _.flatMap(res.Reservations, (r) => r.Instances);
+
+                    function awsToNode(instance) {
+                        /**
+                     * description
+                        | NAME ||
+                        | ZONE ||
+                        | MACHINE_TYPE ||
+                        | INTERNAL_IP ||
+                        | EXTERNAL_IP ||
+                        | STATUS ||
+                     *
+                     */
+                        return {
+                            name: instance.InstanceId,
+                            nodeType: instance.InstanceType,
+                            state: instance.State,
+                            internalIP: instance.PrivateIpAddress,
+                            publicIP: instance.PublicIpAddress
+                        }
+                    }
+
+                    function printCollection(coll) {
+                        console.log("Found %s instances", coll.length);
+                        coll.forEach((c) => console.log(awsToNode(c)));
+                    }
+                    printCollection(instances)
+                }).catch(handleError);
 
             break;
 
         case "status":
             ec2.describeInstances().promise()
-            .then(function(res) {
-                var id = res.Reservations[0].Instances[0].InstanceId;
-                return ec2.describeInstanceStatus({InstanceIds: [id]}).promise();
-            }).then(function(res) {
-                var statuses = res.InstanceStatuses[0]
-                console.log('RRR', statuses);
-            }).catch(handleError);
+                .then(function(res) {
+                    var id = res.Reservations[0].Instances[0].InstanceId;
+                    return ec2.describeInstanceStatus({
+                        InstanceIds: [id]
+                    }).promise();
+                }).then(function(res) {
+                    var statuses = res.InstanceStatuses[0]
+                    console.log('RRR', statuses);
+                }).catch(handleError);
             break;
         case 'sss':
 
